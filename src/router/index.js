fef4cb41921bc305d9c317eadfa82cba9e737864
widem <wangweidongip@163.com>
2019-10-25 14:58:00 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -53,9 +53,7 @@ const router = new Router({
 })
 router.beforeEach((to, from, next) => {
   /* 路由发生变化修改页面title */
-  if (to.meta.title) {
-    document.title = to.meta.title
-  }
+  if (to.meta.title) { document.title = to.meta.title }
   next()
 })
 
